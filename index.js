6a25c648a47588782c392d2feb74e1bf7e77e088
Pawel Konik <konius85@gmail.com>
2017-11-06 20:01:46 +0000

    Poprawka

--- index.js
+++ index.js
@@ -3,34 +3,29 @@ process.stdin.on('readable', function() {
     var input = process.stdin.read();
     if (input !== null) {
         var instruction = input.toString().trim();
-        var os = process.env.os;
+        let nodeVer = process.versions.node;
+        let lang = process.env.LANG;
+        let osType = process.platform;
 
-        // if(os !=== null){
-        // 	process.stdout.write(os);
-        // }else{
-        // 	process.stderr.write("Unknown");
-        // }
-
-        switch (os) {
-        	case "Windows_NT":
-       			process.stdout.write("Windows");
-        		break;
-        	case "Linux":
-       			process.stdout.write("Linux");
-	        	break;
-        	case "macOS":
-	   			process.stdout.write("Macintosh");
-	        	break;
-        	default:
-        		process.stderr.write("Unknown");
-        		break;
+        switch (instruction) {
+            case '/version':
+                process.stdout.write(nodeVer + '\n');
+                break;
+            case '/lang':
+                if((osType == "linux") || (osType == "darvin")){
+                    process.stdout.write(osType + '\n');
+                }else{
+                    process.stderr.write('Bad system \n');
+                }
+                break;
+            case '/exit':
+                process.stdout.write('Goodbye!!!' + '\n');
+                process.exit();
+                break;
+            default:
+                process.stderr.write('Wrong instruction! \n');
+                break
         }
 
-        if (instruction === '/exit') {
-            process.stdout.write('Quitting app!\n');
-            process.exit();
-        } else {
-            process.stderr.write('Wrong instruction!');
-        }
     }
 });
